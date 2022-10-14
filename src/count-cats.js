17b452639219a29bb3d5748e25a3eceafce75a42
AzureAD\IharAntonenka <i_antonenka@locport.io>
2022-10-14 17:21:09 +0300

    update: implement countCats

--- src/count-cats.js
+++ src/count-cats.js
@@ -8,16 +8,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * @example
  * countCats([
- *  [0, 1, '^^'],
+ *  [0, 1, '^^', ],
  *  [0, '^^', 2],
  *  ['^^', 1, 2]
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(matrix) {
+  if(!matrix){
+    return 0;
+  }
+  if(matrix.length === 0) {
+    return 0;
+  }
+  let count = 0;
+
+  for(let i = 0; i < matrix.length; i++) {
+    count += matrix[i].filter(c=>c==='^^').length;;
+  }
+  return count;
 }
+
+// countCats([
+//     [0, 1, '^^', '^^'],
+//     [0, '^^', 2],
+//     ['^^', 1, 2]
+//    ])
 
 module.exports = {
   countCats
