5d1eb8a7dd993dd817adee8c231fb4270e339818
AzureAD\IharAntonenka <i_antonenka@locport.io>
2022-10-15 20:01:18 +0300

    update: recursive-depeth

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -13,11 +13,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+   let maxDepth = 1;
+   //let depth = 1;
+   for (let i = 0; i < arr.length; i++) {
+    if(Array.isArray(arr[i])) {
+      //depth += this.calculateDepth(arr[i]);
+      maxDepth = Math.max(maxDepth, this.calculateDepth(arr[i])+1);
+      //depth = 1;
+    }
+   } 
+   return maxDepth;
   }
 }
+
+const depthCalc = new DepthCalculator();
+
+//let f = depthCalc.calculateDepth([1, 2, 3, 4, 5]);
+let f1 = depthCalc.calculateDepth([1, [8, [[]]], [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[]]]]]]], []]]], []]]]]]]]], []]]], []]]]]]]]]], 2, 3, [8, [[[[[[[[[[[[[[]]]]]]]]]]]]]]], [8, [[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]], 4, 5, ['6575', ['adas', ['dfg', [0]]]]]);
+let f2 = depthCalc.calculateDepth([[[]]]);
 
 module.exports = {
   DepthCalculator
