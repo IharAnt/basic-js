d3db73f3972ee55f4c2b89706872391f63ce59af
AzureAD\IharAntonenka <i_antonenka@locport.io>
2022-10-16 16:39:23 +0300

    update: dns stats

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,10 +22,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  let array = domains.map(d => d.split('.').reverse());
+  let maxdepth = array.reduce((max, c) => max = c.length>max?c.length:max, 0);
+  let result = new Object();
+  for (let i = 1; i <= maxdepth; i++) {
+    let domains = array.filter(a=>a.length>=i).map(a=>'.' + a.slice(0, i).join('.'));
+
+    let sets = new Set(domains);
+    for (const set of sets) {
+      result[set] = domains.filter(d=>d === set).length;
+    }
+  }
+  return result;
 }
+
+let p = getDNSStats(['code.yandex.ru', 'music.yandex.ru', 'yandex.ru'])
 
 module.exports = {
   getDNSStats
