566af8e4610df8762603d54488c50c507fcfb8fa
AzureAD\IharAntonenka <i_antonenka@locport.io>
2022-10-16 04:41:07 +0300

    update: vigenere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,15 +20,80 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+  constructor (isDirectMachine = true) {
+    this.isDirectMachine = isDirectMachine;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+   if(!message || !key){
+    throw new Error('Incorrect arguments!');
+   }
+   let result = '';
+   message = message.toUpperCase();
+   key = key.toUpperCase();
+  
+   for (let i = 0, n = 0; i < message.length; i++, n++) {
+    if (this.alphabet.indexOf(message[i])>=0) {
+      result += this.getEncryptSymbol(message[i], key[n % key.length]);
+    } else {
+      result += message[i];
+      n--;
+    }
+   }
+   if(!this.isDirectMachine) {
+    return Array.from(result).reverse().join('');
+   } else return result;
+
   }
+
+  
+  decrypt(message, key) {
+    if(!message || !key){
+      throw new Error('Incorrect arguments!');
+    }
+    let result = '';
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+  
+    for (let i = 0, n = 0; i < message.length; i++, n++) {
+      if (this.alphabet.indexOf(message[i])>=0) {
+        result += this.getDecryptSymbol(message[i], key[n % key.length]);
+      } else {
+        result += message[i];
+        n--;
+      }
+    }
+    if(!this.isDirectMachine) {
+      return Array.from(result).reverse().join('');
+    } else return result;
+  }
+
+  getMod(n, m) {
+    return ((n % m) + m) % m;
+  }
+
+  getEncryptSymbol(symbol, key) {
+   let p = symbol.charCodeAt(0);
+   let k = key.charCodeAt(0);
+   let c = this.getMod(p + k, 26) + 65;
+   return String.fromCharCode(c);
+  }
+
+  getDecryptSymbol(symbol, key) {
+   let c = symbol.charCodeAt(0);
+   let k = key.charCodeAt(0);
+   let p = this.getMod(c - k, 26) + 65;
+   return String.fromCharCode(p);
+  }
+
 }
+
+const directMachine = new VigenereCipheringMachine(false);
+
+let ff = directMachine.encrypt('cryptography', 'verylongkeyword');
+let f = directMachine.decrypt('XVPNECTXKTFU', 'verylongkeyword');
 
 module.exports = {
   VigenereCipheringMachine
