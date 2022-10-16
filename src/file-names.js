10279bbf33e16910c1b22746331802e8c93be325
AzureAD\IharAntonenka <i_antonenka@locport.io>
2022-10-16 18:49:11 +0300

    update: file names

--- src/file-names.js
+++ src/file-names.js
@@ -15,9 +15,29 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function renameFiles(names) {
+  let result = [];
+  for (const file of names) {
+    let countFiles = result.filter(f => isEqualsFileNames(f, file)).length;
+    if(countFiles>0){
+      result.push(file+`(${countFiles})`);
+    } else {
+      result.push(file);
+    }
+  }
+  return result;
+}
+
+function isEqualsFileNames(nameFromList, name) {
+  if(nameFromList === name) {
+    return true;
+  }
+  let clearName = nameFromList;
+  if(nameFromList.indexOf('(')>-1) {
+    let array = nameFromList.split('(');
+    clearName = array.slice(0, array.length-1).join('(');
+  }
+  return clearName === name;
 }
 
 module.exports = {
